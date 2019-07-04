7553c3fabf768b0071473a96b1d6288527df7af8
asyriac <43615822+asyriac@users.noreply.github.com>
2019-07-04 16:22:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-
+const res = document.querySelector('.feedback');
+const op = document.querySelector('.result');
+const answers = ['A','A','B','B'];
+res.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  score = 0;
+  const userAnswers = [res.q1.value,res.q2.value,res.q3.value,res.q4.value];
+  userAnswers.forEach((a,b)=>{
+    if(a===answers[b]) score++;
+  });
+  op.innerText = `You scored ${score}!`
+});
